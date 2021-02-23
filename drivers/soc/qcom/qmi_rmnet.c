e91602e5bc386d26f0bc84adfc5c01fedddf7d82
LibXZR <xzr467706992@163.com>
2021-02-23 21:14:04 +0800

    qmi_rmnet: Make powersave workqueue unbound and freezable
    * After commit "Revert "dfc: Use alarm timer to trigger powersave work",
    this workqueue is now is use for qmi_rmnet_check_stats. Make it unbound
    and freezable to save power.
    
    Signed-off-by: LibXZR <xzr467706992@163.com>
    Signed-off-by: Divyanshu-Modi <divyan.m05@gmail.com>
    Signed-off-by: Carlos Jimenez (JavaShin-X) <javashin1986@gmail.com>

--- drivers/soc/qcom/qmi_rmnet.c
+++ drivers/soc/qcom/qmi_rmnet.c
@@ -1116,8 +1116,8 @@ void qmi_rmnet_work_init(void *port)
 	if (rmnet_ps_wq)
 		return;
 
-	rmnet_ps_wq = alloc_workqueue("rmnet_powersave_work",
-					WQ_MEM_RECLAIM | WQ_CPU_INTENSIVE, 1);
+	rmnet_ps_wq = create_freezable_workqueue("rmnet_powersave_work",
+					WQ_MEM_RECLAIM);
 
 	if (!rmnet_ps_wq)
 		return;
