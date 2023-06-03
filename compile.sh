10d4c3eae9caf2480602cea62c1cf090fed3a2e6
Alexey Shelby <shelbyhell@proton.me>
2023-06-03 06:12:14 +0000

    compile: Update script for build by GCC.
    Signed-off-by: Alexey Shelby <shelbyhell@proton.me>

--- compile.sh
+++ compile.sh
@@ -1,44 +1,70 @@
 #!/bin/bash
 #
-# Compile script for QuicksilveR kernel
+# Compile script for ? kernel
 # Copyright (C) 2020-2021 Adithya R.
 
 SECONDS=0 # builtin bash timer
-ZIPNAME="SunriseKernel-juice-$(date '+%Y%m%d-%H%M').zip"
-TC_DIR="$HOME/tc/sdclang"
-GCC_DIR="$HOME/tc/gcc"
-GCC64_DIR="$HOME/tc/gcc64"
+ZIPNAME="/tmp/output/SunriseTestAdreno-juice_$(date +%Y%m%d-%H%M).zip"
+DTC_DIR="$HOME/tc/dtc"
 AK3_DIR="$HOME/android/AnyKernel3"
 DEFCONFIG="vendor/bengal-perf_defconfig"
 
-export PATH="${TC_DIR}/compiler/bin:${GCC64_DIR}/bin:${GCC_DIR}/bin:/usr/bin:${PATH}"
+mkdir -p /tmp/output
 
-if ! [ -d "$TC_DIR" ]; then
-echo "sdclang not found! Cloning to $TC_DIR..."
-if ! git clone -q -b 14 --depth=1 https://github.com/ThankYouMario/proprietary_vendor_qcom_sdclang $TC_DIR; then
-echo "Cloning failed! Aborting..."
-exit 1
-fi
-fi
+rel=0
 
-if ! [ -d "$GCC_DIR" ]; then
-echo "GCC not found! Cloning to $GCC_DIR..."
-if ! git clone -q -b master --depth=1 https://github.com/Enprytna/arm-linux-androideabi-4.9 $GCC_DIR; then
-echo "Cloning failed! Aborting..."
-exit 1
-fi
-fi
+env() {
+export TELEGRAM_BOT_TOKEN=""
+export TELEGRAM_CHAT_ID="@SunriseCI"
 
-if ! [ -d "$GCC64_DIR" ]; then
-echo "GCC64 not found! Cloning to $GCC64_DIR..."
-if ! git clone -q -b master --depth=1 https://github.com/Enprytna/aarch64-linux-android-4.9 $GCC64_DIR; then
-echo "Cloning failed! Aborting..."
-exit 1
-fi
-fi
+TRIGGER_SHA="$(git rev-parse HEAD)"
+LATEST_COMMIT="$(git log --pretty=format:'%s' -1)"
+COMMIT_BY="$(git log --pretty=format:'by %an' -1)"
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+KERNEL_VERSION="$(cat out/.config | grep Linux/arm64 | cut -d " " -f3)"
+export FILE_CAPTION="
+Linux version: $KERNEL_VERSION
+Branch: $BRANCH
+Top commit: $LATEST_COMMIT
+Commit author: $COMMIT_BY
+Status: $STATUS"
+}
 
+############# Build status.
+export RELEASE=$rel
+if [ "${RELEASE}" == 1 ]; then
+    export STATUS="Release"
+else
+    export STATUS="Bleeding-Edge"
+fi
+#############
+
+############# Needed variables.
+export KDIR=$(pwd)
+export LINKER="ld"
+export PATH="${KDIR}"/gcc32/bin:"${KDIR}"/gcc64/bin:/usr/bin/:${PATH}
+export DTC_EXT="${DTC_DIR}/linux-x86/dtc/dtc"
 export KBUILD_BUILD_USER=ShelbyHell
-export KBUILD_BUILD_HOST=SunriseCI
+export KBUILD_BUILD_HOST=Instance
+#############
+
+if [ ! -d "${KDIR}/gcc64" ]; then
+        curl -sL https://github.com/cyberknight777/gcc-arm64/archive/refs/heads/master.tar.gz | tar -xzf -
+        mv "${KDIR}"/gcc-arm64-master "${KDIR}"/gcc64
+fi
+
+if [ ! -d "${KDIR}/gcc32" ]; then
+	curl -sL https://github.com/cyberknight777/gcc-arm/archive/refs/heads/master.tar.gz | tar -xzf -
+        mv "${KDIR}"/gcc-arm-master "${KDIR}"/gcc32
+fi
+
+if ! [ -d "$DTC_DIR" ]; then
+echo "DTC not found! Cloning to $DTC_DIR..."
+if ! git clone -q -b android10-gsi --depth=1 https://android.googlesource.com/platform/prebuilts/misc $DTC_DIR; then
+echo "Cloning failed! Aborting..."
+exit 1
+fi
+fi
 
 if [[ $1 = "-r" || $1 = "--regen" ]]; then
 make O=out ARCH=arm64 $DEFCONFIG savedefconfig
@@ -53,8 +79,25 @@ fi
 mkdir -p out
 make O=out ARCH=arm64 $DEFCONFIG
 
+env
+
+curl -s "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendmessage" \
+    -d "text=<code>Start build kernel for Redmi 9T/POCO M3</code>" \
+    -d "chat_id=${TELEGRAM_CHAT_ID}" -d "parse_mode=HTML"
+
 echo -e "\nStarting compilation...\n"
-make -j$(nproc --all) O=out ARCH=arm64 LD_LIBRARY_PATH="${TC_DIR}/lib:${LD_LIBRARY_PATH}" CC=clang LD=ld.lld AR=llvm-ar AS=llvm-as NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip CROSS_COMPILE=aarch64-linux-android- CROSS_COMPILE_ARM32=arm-linux-androideabi- CLANG_TRIPLE=aarch64-linux-gnu- Image
+make -j16 \
+    ARCH=arm64 \
+    O=out \
+    CROSS_COMPILE=aarch64-elf- \
+    CROSS_COMPILE_ARM32=arm-eabi- \
+    LD="${KDIR}"/gcc64/bin/aarch64-elf-"${LINKER}" \
+    AR=aarch64-elf-ar \
+    AS=aarch64-elf-as \
+    NM=aarch64-elf-nm \
+    OBJDUMP=aarch64-elf-objdump \
+    OBJCOPY=aarch64-elf-objcopy \
+    CC=aarch64-elf-gcc Image dtbo.img 2>&1 | tee log.txt
 
 if [ -f "out/arch/arm64/boot/Image" ]; then
 echo -e "\nKernel compiled succesfully! Zipping up...\n"
@@ -65,19 +108,22 @@ echo -e "\nAnyKernel3 repo not found locally and cloning failed! Aborting..."
 exit 1
 fi
 cp out/arch/arm64/boot/Image AnyKernel3
+cp out/arch/arm64/boot/dtbo.img AnyKernel3
 rm -f *zip
 cd AnyKernel3
 git checkout master &> /dev/null
-zip -r9 "../$ZIPNAME" * -x '*.git*' README.md *placeholder
+zip -r9 "$ZIPNAME" * -x '*.git*' README.md *placeholder
 cd ..
 rm -rf AnyKernel3
 rm -rf out/arch/arm64/boot
 echo -e "\nCompleted in $((SECONDS / 60)) minute(s) and $((SECONDS % 60)) second(s) !"
 echo "Zip: $ZIPNAME"
 if ! [[ $HOSTNAME = "enprytna" && $USER = "endi" ]]; then
-curl --upload-file $ZIPNAME https://temp.sh/$ZIPNAME; echo
+curl -F document=@"${ZIPNAME}" -F "caption=${FILE_CAPTION}" "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendDocument?chat_id=${TELEGRAM_CHAT_ID}&parse_mode=Markdown"
+curl -F document=@"log.txt" -F "caption=Nice work!" "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendDocument?chat_id=${TELEGRAM_CHAT_ID}&parse_mode=Markdown"
 fi
 else
 echo -e "\nCompilation failed!"
+curl -F document=@"log.txt" -F "caption=ERROR!" "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/sendDocument?chat_id=${TELEGRAM_CHAT_ID}&parse_mode=Markdown"
 exit 1
 fi
